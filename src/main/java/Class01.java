ad3e22b98f3622875060caf5dac91d52676a1b8e
Selma Simsek <selmasimsek0406@gmail.com>
2023-08-24 08:55:04 +0200

    versiyon2

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -6,6 +6,14 @@ public class Class01 {
         int y = 2 ;
         System.out.println(x+y);
 
+        //versiyon 2
+        int z = 5;
+        System.out.println(x+y+z);
+
+
+
+        
+
 
     }
 }
